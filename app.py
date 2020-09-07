4588f90a042bf9cf8d3dcf0ed005c07bceeecf0f
Gunasekaran92 <35014357+Gunasekaran92@users.noreply.github.com>
2020-09-07 15:32:36 -0400

    Update app.py
    removed cors

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, make_response, request, abort
 import pandas as pd
 import pickle
-from flask_cors import CORS,cross_origin
+#from flask_cors import CORS,cross_origin
 model = pickle.load(open( "finalized_model.sav", "rb"))
 
 
